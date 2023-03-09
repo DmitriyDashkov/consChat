41fe8c1b9c686f587c8fdc0b8d95650227f8cde0
Dmitriy <dashkov-d@yandex.ru>
2023-03-09 11:35:37 +0300

    Update file

--- chat.cpp
+++ chat.cpp
@@ -12,3 +12,16 @@ void Chat::addUser(User* newUser)
     numberOfUsers++;
     cout << newUser->getUserLogin() << " entered the chat" << endl;
 }
+
+void Chat::showUsers()
+{
+    cout << "Now in chat: " << endl;
+    for(int i=0; i<numberOfUsers; i++) {
+        cout << i << " - " << users[i]->getUserLogin() << endl;
+    }
+}
+
+void Chat::setCurrentUser(User* currUser)
+{
+    currentUser = currUser;
+}
