8912fdda01e20b69293b9969bc3116af2e2801a5
SvetLanaF821 <fomoidi_sv@mail.ru>
2023-03-09 17:44:53 +0300

    Add getters in file Message.h

--- Message.h
+++ Message.h
@@ -13,5 +13,10 @@ public:
 	Message(const std::string& from, const std::string& to, const std::string& message);
 	~Message();
 
-	
+	std::string& getFrom() const;
+	void setFrom(std::string& from);
+	std::string& getTo() const;
+	void setTo(std::string& to);
+	std::string& getMessage() const;
+	void setMessage(std::string& message);
 };
