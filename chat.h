2025d362145d202c863b201ac6cf9b04d1630c53
Dmitriy <dashkov-d@yandex.ru>
2023-03-09 11:35:23 +0300

    Update file

--- chat.h
+++ chat.h
@@ -16,6 +16,7 @@ public:
     Chat(string n);
     int numberOfUsers {0};          //количество юзеров в чате
     User* users[MAX_USERS];         //список юзеров
+    void showUsers();
     void addUser(User* newUser);    //добавить юзера в чат
     User* currentUser;              //текущий пользователь чата
     void setCurrentUser (User* u);  //установить текущего пользователя чата
