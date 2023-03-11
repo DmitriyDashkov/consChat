fff7b1ddc6c23bba3eb34924435c10ba8ea2507f
Greg Farakhutdinov <farahutdinov@yandex.ru>
2023-03-11 11:13:05 +0500

    remove using namespace

--- user.h
+++ user.h
@@ -1,39 +1,38 @@
 #pragma once
 #include <string>
-using namespace std;
 
 class User
 
 {
-    const string login_; // нельзя поменть логин (const)
-    string password_; // можно поменять пароль
-    string name_; // можно поменять имя
+    const std::string login_; // нельзя поменть логин (const)
+    std::string password_; // можно поменять пароль
+    std::string name_; // можно поменять имя
 
 public:
-    User(const string& login, const string& password, const string& name) : login_(login), password_(password), name_(name)
+    User(const std::string& login, const std::string& password, const std::string& name) : login_(login), password_(password), name_(name)
     {}
 
-    const string& getUserLogin() const
+    const std::string& getUserLogin() const
 
     {
         return login_;
     }
 
-    const string& getUserPassword() const
+    const std::string& getUserPassword() const
 
     {
         return password_;
     }
 
-    void setUserPassword(const string& password)
+    void setUserPassword(const std::string& password)
 
     {
         password_ = password;
     }
 
-    const string& getUserName() const { return name_; }
+    const std::string& getUserName() const { return name_; }
 
-    void setUserName(const string& name)
+    void setUserName(const std::string& name)
 
     {
         name_ = name;
