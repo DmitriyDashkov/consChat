766ce4949ee35e72549946444b177ca8f1ac4dd0
Greg Farakhutdinov <farahutdinov@yandex.ru>
2023-03-12 14:06:06 +0500

    update user.h (styling)

--- user.h
+++ user.h
@@ -2,40 +2,19 @@
 #include <string>
 
 class User
-
 {
     const std::string _login; // нельзя поменть логин (const)
     std::string _password; // можно поменять пароль
     std::string _name; // можно поменять имя
 
 public:
-    User(const std::string& login, const std::string& password, const std::string& name) : _login(login), _password(password), _name(name)
-    {}
-
-    const std::string& getUserLogin() const
-
-    {
-        return _login;
-    }
-
-    const std::string& getUserPassword() const
-
-    {
-        return _password;
-    }
-
-    void setUserPassword(const std::string& password)
-
-    {
-        _password = password;
-    }
-
-    const std::string& getUserName() const { return _name; }
-
-    void setUserName(const std::string& name)
-
-    {
-        _name = name;
-    }
-
+    User();
+    User(const std::string& login, const std::string& password, const std::string& name);
+    ~User();
+    
+    const std::getUserLogin() const;
+    const std::getUserPassword() const;
+    void setUserPassword(std::string& password);
+    const std::getUserName() const;
+    void setUserName(std::string& name);  
 };
