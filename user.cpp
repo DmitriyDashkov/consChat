d8180570b114c1b91928afd1f76c96402b94b68b
Greg Farakhutdinov <farahutdinov@yandex.ru>
2023-03-12 13:55:26 +0500

    update user.cpp

--- user.cpp
+++ user.cpp
@@ -1,2 +1,11 @@
 #include "user.h"
 
+User::User() = default;
+User::User(const std::string & login, const std::string & password, const std::string & name) : _login(login), _password(password), _name(name) {}
+User::~User() {}
+
+const std::string& getUserLogin() const { return _login; }
+const std::string& getUserPassword() const { return _password; }
+void setUserPassword(const std::string& password) { _password = password; }
+const std::string& getUserName() const { return _name; }
+void setUserName(const std::string& name) { _name = name; }
