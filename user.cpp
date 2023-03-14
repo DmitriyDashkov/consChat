df613a393dbc6f2ab3188a65894e6e4edc075846
DmitriyDashkov <126846692+DmitriyDashkov@users.noreply.github.com>
2023-03-14 09:09:59 +0300

    Add files via upload

--- user.cpp
+++ user.cpp
@@ -4,8 +4,8 @@ User::User() = default;
 User::User(const std::string & login, const std::string & password, const std::string & name) : _login(login), _password(password), _name(name) {}
 User::~User() {}
 
-const std::string& getUserLogin() const { return _login; }
-const std::string& getUserPassword() const { return _password; }
-void setUserPassword(const std::string& password) { _password = password; }
-const std::string& getUserName() const { return _name; }
-void setUserName(const std::string& name) { _name = name; }
+std::string User::getUserLogin() const { return _login; }
+std::string User::getUserPassword() const { return _password; }
+void User::setUserPassword(std::string& password) { _password = password; }
+std::string User:: getUserName() const { return _name; }
+void User::setUserName(std::string& name) { _name = name; }
--- user.h
+++ user.h
@@ -1,62 +1,19 @@
 #pragma once
 #include <string>
-
 class User
 {
-    const std::string _login; // Ð½ÐµÐ»ÑÐ·Ñ Ð¿Ð¾Ð¼ÐµÐ½ÑÑ Ð»Ð¾Ð³Ð¸Ð½ (const)
-    std::string _password; // Ð¼Ð¾Ð¶Ð½Ð¾ Ð¿Ð¾Ð¼ÐµÐ½ÑÑÑ Ð¿Ð°ÑÐ¾Ð»Ñ
-    std::string _name; // Ð¼Ð¾Ð¶Ð½Ð¾ Ð¿Ð¾Ð¼ÐµÐ½ÑÑÑ Ð¸Ð¼Ñ
+    const std::string _login;
+    std::string _password;
+    std::string _name;
 
 public:
     User();
     User(const std::string& login, const std::string& password, const std::string& name);
     ~User();
     
-    const std::getUserLogin() const;
-    const std::getUserPassword() const;
+    std::string getUserLogin() const;
+    std::string getUserPassword() const;
     void setUserPassword(std::string& password);
-    const std::getUserName() const;
+    std::string getUserName() const;
     void setUserName(std::string& name);  
 };
-    const string login_; // íåëüçÿ ïîìåíòü ëîãèí (const)
-    string password_; // ìîæíî ïîìåíÿòü ïàðîëü
-    string name_; // ìîæíî ïîìåíÿòü èìÿ
-
-public:
-    User(const string& login, const string& password, const string& name) : login_(login), password_(password), name_(name)
-    {}
-
-    const string& getUserLogin() const
-
-    {
-        return login_;
-    }
-
-    const string& getUserPassword() const
-
-    {
-        return password_;
-    }
-
-    const string& getUserName() const
-
-    {
-        return name_;
-    }
-
-    void setUserPassword(const string& password)
-
-    {
-        password_ = password;
-    }
-
-    const string& setUserName() const { return name_; }
-
-    void setUserName(const string& name)
-
-    {
-        name_ = name;
-    }
-
-};
-
